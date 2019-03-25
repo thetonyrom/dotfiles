bcddabd3226f9d6c23e71df9b1bd0e5240c44651
Tony Rom <thetonyrom@gmail.com>
2019-03-25 18:29:41 +0300

    Initial main configuration

---
+++ configure.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Xorg
+sudo ln -snf $DOT_DIR/xorg.conf.d /etc/X11/xorg.conf.d
+
+# Xresources
+ln -sf $DOT_DIR/Xresources $HOME/.Xresources
+
+# i3
+ln -snf $DOT_DIR/i3 $XDG_CONFIG_HOME/i3
+
+# zsh
+ln -sf $DOT_DIR/zshrc $HOME/.zshrc
+
+# neovim
+ln -snf $DOT_DIR/nvim $XDG_CONFIG_HOME/nvim
+
+# vim
+ln -sf $DOT_DIR/vimrc $HOME/.vimrc
